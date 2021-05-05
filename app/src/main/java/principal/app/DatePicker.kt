24182a97a0e09ab7b2e56db44893983bd20e5b6f
Kevin <kagn.521@gmail.com>
2021-05-05 12:24:17 -0600

    Arreglando date picker

---
+++ app/src/main/java/principal/app/DatePicker.kt
@@ -0,0 +1,28 @@
+package principal.app
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class DatePicker(val listener: (day: Int, month: Int, year: Int) -> Unit) : DialogFragment(),
+    DatePickerDialog.OnDateSetListener {
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
+        listener(day, month, year)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendar = Calendar.getInstance()
+
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val month = calendar.get(Calendar.MONTH)
+        val year = calendar.get(Calendar.YEAR)
+
+        return DatePickerDialog(activity as Context, this, year, month, day)
+    }
+}
+
--- app/src/main/java/principal/app/HomeFragment.kt
+++ app/src/main/java/principal/app/HomeFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -27,6 +30,21 @@ class HomeFragment : Fragment() {
         adapter = RecyclerAdapter()
         recyclerView.adapter = adapter
 
+        view.findViewById<EditText>(R.id.date_picker_1).setOnClickListener { showDatePickerDialog() }
+        view.findViewById<EditText>(R.id.date_picker_2).setOnClickListener { showDatePickerDialog() }
+
         return view
+    }
+
+    private fun showDatePickerDialog() {
+        val datePicker = DatePicker { day, month, year ->
+            onDateSelected(day, month, year)
+        }
+
+        datePicker.show(parentFragmentManager, "datePicker")
+    }
+
+    private fun onDateSelected(day: Int, month: Int, year: Int) {
+        date_picker_1.setText("$day $month $year")
     }
 }
