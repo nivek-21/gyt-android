b5a4185b1fc25743912d67aa5b8ee42a4899e166
Kevin <kagn.521@gmail.com>
2021-05-05 08:38:13 -0600

    Saving...

--- app/src/main/java/principal/app/LoginFragment.kt
+++ app/src/main/java/principal/app/LoginFragment.kt
@@ -1,15 +1,30 @@
 package principal.app
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 class LoginFragment : Fragment() {
+
+    private val loginTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            login_btn.isEnabled = email_input.text.isNotEmpty() && password_input.text.isNotEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,16 +32,13 @@ class LoginFragment : Fragment() {
     ): View? {
         val fragment = inflater.inflate(R.layout.fragment_login, container, false)
 
-        fragment.findViewById<Button>(R.id.register_btn).setOnClickListener {
+        fragment.register_btn.setOnClickListener {
             Navigation.findNavController(fragment)
                 .navigate(R.id.action_fragment_login_to_fragment_register)
         }
 
-        // val emailInput = fragment.findViewById<EditText>(R.id.email_input)
-        // val passwordInput = fragment.findViewById<EditText>(R.id.password_input)
-        val loginBtn: Button = fragment.findViewById<Button>(R.id.login_btn)
-
-        loginBtn.isEnabled = false
+        fragment.email_input.addTextChangedListener(loginTextWatcher)
+        fragment.password_input.addTextChangedListener(loginTextWatcher)
 
         return fragment
     }
