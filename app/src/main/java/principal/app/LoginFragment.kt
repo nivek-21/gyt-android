3e7c7b678c2cfd3ca9d8d606cfc133d3fad1a90c
carlos <k.ike08@hotmail.com>
2021-05-18 08:36:36 -0600

    kike

--- app/src/main/java/principal/app/LoginFragment.kt
+++ app/src/main/java/principal/app/LoginFragment.kt
@@ -3,6 +3,7 @@ package principal.app
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_login.view.*
+import java.util.regex.Pattern
 
 class LoginFragment : Fragment() {
 
@@ -18,11 +20,41 @@ class LoginFragment : Fragment() {
         }
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            login_btn.isEnabled = email_input.text.isNotEmpty() && password_input.text.isNotEmpty()
+           login_btn.isEnabled = email_input.text.isNotEmpty() && password_input.text.isNotEmpty()
+
+
         }
 
         override fun afterTextChanged(s: Editable?) {
         }
+    }
+
+    private val passwordTextWatcher = object : TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            if(validate(password_input.text.toString()))
+                login_btn.isEnabled = true
+            else{
+
+                login_btn.isEnabled = false
+                password_input.setError("Invalid Regex")
+            }
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+        }
+
+
+    }
+
+
+    private fun validate(text: String?): Boolean {
+        var p = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%!\\-_?&])(?=\\S+\$).{8,}")
+        var m = p.matcher(text)
+        Log.i("test",m.matches().toString())
+        return m.matches()
     }
 
     override fun onCreateView(
@@ -38,7 +70,7 @@ class LoginFragment : Fragment() {
         }
 
         fragment.email_input.addTextChangedListener(loginTextWatcher)
-        fragment.password_input.addTextChangedListener(loginTextWatcher)
+        fragment.password_input.addTextChangedListener(passwordTextWatcher)
 
         return fragment
     }
